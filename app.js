0409ab7541a71355cb1a2eab00e05f8b7f160b70
houyuli <houyuli@jd.com>
2019-07-01 18:14:18 +0800

    代码更新

--- app.js
+++ app.js
@@ -5,43 +5,53 @@
 App({
   onLaunch: function () {
 
-    // 展示本地存储能力
-    // var logs = wx.getStorageSync('logs') || []
-    // logs.unshift(Date.now())
-    // wx.setStorageSync('logs', logs)
+    this.getCityData()
 
     // 登录
     wx.login({
-      success: res => {
-        // isLogin.then(res => {
-        //   console.log('是否登录成功')
-        // })
-      }
+      success: res => {}
     })
+
+    // 获取
 
     // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+    // wx.getSetting({
+    //   success: res => {
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //       wx.getUserInfo({
+    //         success: res => {
+    //           // 可以将 res 发送给后台解码出 unionId
+    //           this.globalData.userInfo = res.userInfo
+    //
+    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //           // 所以此处加入 callback 以防止这种情况
+    //           if (this.userInfoReadyCallback) {
+    //             this.userInfoReadyCallback(res)
+    //           }
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
 
 
   },
+  getCityData () {
+    // 获取高德地图的城市数据，并缓存到本地， 如果本地已存在城市数据则不再请求
+    if (!wx.getStorageSync('citys')) {
+      wx.request({
+        url: "https://restapi.amap.com/v3/config/district?subdistrict=3&key=bb4198a1f146184af53322d424732f6b",
+        method: "GET",
+        success: function (res) {
+          console.log(res['data']['districts'][0]['districts']);
+          //  请求到数据 存在本地
+          wx.setStorageSync('citys', res['data']['districts'][0]['districts']);
+        }
+      })
+    }
+  }
+
   globalData: {
     userInfo: null,
     env: 'pre',
---
+++ components/CityPicker/CityPicker.js
@@ -0,0 +1,94 @@
+// components/Button/Button.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    value: {
+      type: Object,
+      value: {
+        province: '',
+        city: '',
+        county: '',
+      },
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    provinces: [],
+    province: "北京市",
+    citys: [],
+    city: "北京城区",
+    countys: [],
+    county: "昌平区",
+    value: [0, 0, 0],
+    isShow: false
+  },
+
+  lifetimes: {
+    attached() {
+      let citys = cityData['districts'][0]['districts'];
+      citys.sort((a, b) => {
+        if (a['adcode'] - 0 < b['adcode'] - 0) {
+          return -1;
+        }
+        if (a['adcode'] - 0 > b['adcode'] - 0) {
+          return 1;
+        }
+        return 0;
+      });
+      this.setData({
+        provinces: citys,
+        citys: citys[0]['districts'],
+        countys: citys[0]['districts'][0]['districts']
+      });
+    },
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    bindChange(e) {
+      const val = e.detail.value;
+      this.setData({
+        value: val,
+        citys: this.data.provinces[val[0]]['districts'].length > 0 ? this.data.provinces[val[0]]['districts'] : [],
+        countys: this.data.provinces[val[0]]['districts'].length > 0 ? (this.data.provinces[val[0]]['districts'][val[1]] ? this.data.provinces[val[0]]['districts'][val[1]]['districts'] : []) : ''
+      })
+    },
+    closeModal() {
+      this.setData({
+        isShow: false
+      })
+    },
+    togglePicker() {
+      this.setData({
+        isShow: !this.data.isShow
+      })
+    },
+    cancel() {
+      this.setData({
+        isShow: false
+      })
+    },
+    done() {
+      const val = this.data.value;
+      this.setData({
+        province: this.data.provinces[val[0]].name,
+        city: this.data.provinces[val[0]]['districts'].length > 0 ? this.data.provinces[val[0]]['districts'][val[1]].name : '',
+        county: this.data.provinces[val[0]]['districts'].length > 0 ? (this.data.provinces[val[0]]['districts'][val[1]]['districts'][val[2]] ? this.data.provinces[val[0]]['districts'][val[1]]['districts'][val[2]].name : '') : '',
+        isShow: false
+      });
+      this.triggerEvent('selected', {
+        province: this.data.province,
+        city: this.data.city,
+        county: this.data.county
+      })
+    }
+  }
+})
